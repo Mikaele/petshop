1267a928007a26815320b27675e64ebf9a15c0f1
Mikaele <Mikaele@Mikaele-PC>
2015-06-03 13:04:43 -0300

    Cadastrando animal

--- src/Controller/AgendaController.java
+++ src/Controller/AgendaController.java
@@ -4,10 +4,25 @@
  */
 package Controller;
 
+import Model.Agenda;
+import java.util.ArrayList;
+
 /**
  *
  * @author info206
  */
-public class AgendaController {
+public class AgendaController { 
+    //cadastra um serviço no banco de dados
+    public void agendar(Agenda ag){
     
+    }
+    //lista todos os serviços
+    public ArrayList getAll(){
+        return null;
+    
+    }
+    //lista todos os serviços por horário
+    public  ArrayList getAllByHorario(){
+        return null;
+    }
 }
--- src/Controller/ServicoController.java
+++ src/Controller/ServicoController.java
@@ -4,10 +4,62 @@
  */
 package Controller;
 
+import Model.Servico;
+import View.Util;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author info206
  */
 public class ServicoController {
+    
+        public void inserirServico(Servico s ){
+            
+        }
+        
+            /*  Essa função tem por finalidade popular o Combobox*/
+    public Vector  getNomes(){
+                   Vector s =new Vector();
+        try {
+            Util util= new Util();
+            Connection conexao = util.conecta();
+            String sql= "Select * from Servico";
+                Statement statement = conexao.createStatement();
+          ResultSet result = statement.executeQuery(sql);
+          while (result.next()){               
+               s.add(result.getString("Nome"));
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(DonoController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return s;
+    }
+
+    
+        public int  getIdByNome(String nome){
+        int id=-1;
+            try {
+            Util util= new Util();
+            Connection conexao = util.conecta();
+            String sql= "Select ID from Servico where Nome like '"+nome+"'";
+                Statement statement = conexao.createStatement();
+          ResultSet result = statement.executeQuery(sql);
+           while (result.next()){               
+               id=result.getInt("ID");
+            }
+       } catch (SQLException ex) {
+            Logger.getLogger(DonoController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return id;
+    }        
+
     
 }
