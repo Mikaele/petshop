81f6a70f699e3dd21281446dd78c15cbfecafa03
Mikaele <Mikaele@Mikaele-PC>
2015-06-02 23:11:52 -0300

    inicial

--- src/Model/Animal.java
+++ src/Model/Animal.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -6,8 +7,87 @@ package Model;
 
 /**
  *
- * @author info206
+ * @author orientador
  */
 public class Animal {
+    int id, donoId;
+    String nome, cor, raca, tamanho, obs, nascimento;
+
+    public Animal(int id, int donoId, String nome, String cor, String raca, String tamanho, String obs, String nascimento) {
+        this.id = id;
+        this.donoId = donoId;
+        this.nome = nome;
+        this.cor = cor;
+        this.raca = raca;
+        this.tamanho = tamanho;
+        this.obs = obs;
+        this.nascimento = nascimento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDonoId() {
+        return donoId;
+    }
+
+    public void setDonoId(int donoId) {
+        this.donoId = donoId;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public String getObs() {
+        return obs;
+    }
+
+    public void setObs(String obs) {
+        this.obs = obs;
+    }
+
+    public String getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(String nascimento) {
+        this.nascimento = nascimento;
+    }
+    
+    
     
 }
--- src/Model/Dono.java
+++ src/Model/Dono.java
@@ -1,13 +1,77 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package Model;
 
 /**
  *
- * @author info206
+ * @author orientador
  */
 public class Dono {
+     int id;
+    String nome,nascimento,cpf,endereco,telefone;
+
+    public Dono(int id, String nome, String nascimento, String cpf, String endereco, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.nascimento = nascimento;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    public Dono(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
     
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(String nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+    
+
 }
